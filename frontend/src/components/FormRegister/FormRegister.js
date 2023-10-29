75bb076f64cde662fc5a1796a104e37559b4026a
victoralmeida428 <victor.almeida@unigranrio.br>
2023-10-29 18:08:12 -0300

    response

---
+++ frontend/src/components/FormRegister/FormRegister.js
@@ -0,0 +1,10 @@
+import { Form } from 'react-bootstrap'
+import './form.css'
+
+export default function FormRegister() {
+        return (
+            <Form>
+                
+            </Form>
+        )
+}
