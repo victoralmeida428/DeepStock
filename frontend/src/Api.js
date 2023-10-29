5110976421e5a85e0a7b3e7d14827f21c8d2a119
Victor Gomes <117787474+victoralmeida428@users.noreply.github.com>
2023-10-28 23:37:14 -0300

    Update Api.js

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
-const api = axios.create({baseURL: 'http://localhost:8000/api/v1/', })
+const api = axios.create({baseURL: 'https://victor428.pythonanywhere.com/api/v1/', })
 
 export default api
